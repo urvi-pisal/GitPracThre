5d4fe77e5e8b89ecf9c8cc65b10479bcde2cd725
urvi-pisal <31260127+urvi-pisal@users.noreply.github.com>
2019-05-20 13:48:19 -0400

    Add files via upload

---
+++ praccC.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class praccC {
+	
+	public static void main (String[] args) {
+		Random m = new Random();
+		System.out.println("Give me your favorite smallest integer?");
+		Scanner in = new Scanner(System.in);
+		int x = in.nextInt();
+		int y = in.nextInt();
+		System.out.println("Give me your favorite largest integer?");
+		System.out.println("Now selecting a random number from " + x + " to " + y + " : " + (nextInt(x-y) + y));
+		
+		
+
+	}
+
+}
